bb32cb25bf0c50800c5bdf2bf5235ee56fdc7757
Pius Aaron <piuschbz@gmail.com>
2024-08-06 21:57:34 +0100

    Fixed linter error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,26 +6,27 @@
 
 const { argv } = require('process');
 const request = require('request');
-url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 
 request.get(url, async (err, res, body) => {
-  if (err){
+  if (err) {
     console.log(err);
   }
 
-  const characters = JSON.parse(body).characters;
-  for (const character_url of characters){
+  const { characters } = JSON.parse(body);
+  for (const characterURL of characters) {
     await new Promise((resolve, reject) => {
-      request.get(character_url, (err, res, body) => {
-        if (err){
+      request.get(characterURL, (err, res, body) => {
+        if (err) {
           console.log(err);
           reject();
         }
-        if (res){
+        if (res) {
           console.log(JSON.parse(body).name);
           resolve();
         }
-      })
-    })
+      });
+    });
   }
-})
+});
